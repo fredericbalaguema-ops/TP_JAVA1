f3fc117398b5f2721ca2c41951034530e7a19be9
Frédéric BALAGUEMA <fredericbalaguema@gmail.com>
2026-02-27 13:59:53 +0100

    Add SommeDeuxNombres class to calculate sum

---
+++ SommeDeuxNombres.java
@@ -0,0 +1,19 @@
+public class SommeDeuxNombres {
+
+    public static void main(String[] args) {
+
+        // Déclaration des deux nombres
+        int nombre1 = 10;
+        int nombre2 = 5;
+
+        // Affichage des nombres
+        System.out.println("Premier nombre : " + nombre1);
+        System.out.println("Deuxième nombre : " + nombre2);
+
+        // Calcul de la somme
+        int somme = nombre1 + nombre2;
+
+        // Affichage du résultat
+        System.out.println("La somme des deux nombres est : " + somme);
+    }
+}
