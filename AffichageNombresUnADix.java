a0561169b00dde0369d81524edf0d86feb54057b
Frédéric BALAGUEMA <fredericbalaguema@gmail.com>
2026-02-27 14:19:06 +0100

    Add AffichageNombresUnADix class to display numbers 1-10

---
+++ AffichageNombresUnADix.java
@@ -0,0 +1,11 @@
+public class AffichageNombresUnADix {
+
+    public static void main(String[] args) {
+
+        // Boucle for pour afficher les nombres de 1 à 10
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+
+    }
+}
